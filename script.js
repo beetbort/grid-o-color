d5de35baf0ae3f9aaa8460445848d155a47ba9bf
Nissa <nmnissa@gmail.com>
2020-05-12 11:48:53 -0700

    Handled button click

--- script.js
+++ script.js
@@ -7,10 +7,35 @@ function changeColor(event) {
      
 }
 
-function createGrid() { 
-    let gridSize = 16; 
+function handleClick() { 
+    console.log("clicked"); 
+    let gridSize = prompt("Enter grid size.");
+    if (!gridSize) {
+        return;
+    }
+    gridSize = parseInt(gridSize, 10); 
+    if (isNaN(gridSize) || gridSize <= 0 || gridSize > 100) { 
+        alert("You must enter a number between 1 and 100."); 
+    } else {  
+        removeGrid(); 
+        createGrid(gridSize);
+    }
+    
+}
 
+function removeGrid() { 
+    const container = document.querySelector(".container");
+    while(container.firstChild) { 
+        container.removeChild(container.firstChild); 
+    }
+}
+
+function createGrid(gridSize) { 
+    console.log("grid size", gridSize)
     const container = document.querySelector(".container"); 
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
     for (let i = gridSize * gridSize; i > 0; i--) { 
         container.appendChild(document.createElement("div")); 
     } 
@@ -19,6 +44,12 @@ function createGrid() {
     divs.forEach(div => div.addEventListener("mouseover", changeColor));
 }
 
-createGrid(); 
+function setupButtonListener() { 
+    const newButton = document.querySelector("button"); 
+    newButton.addEventListener("click", handleClick); 
+}
+
+setupButtonListener(); 
+createGrid(16); 
 
 
